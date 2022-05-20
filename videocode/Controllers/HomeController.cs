a9bdcda26223c28d2ad1074f3ac4deeeb680c5e8
Hemalatha <Hemalatha@DESKTOP-UQDUOVD>
2022-05-20 12:25:12 +0530

    Add project files.

---
+++ videocode/Controllers/HomeController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using videocode.Models;
+
+namespace videocode.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.FirstName = "itsolutions";
+            ViewBag.LastName = "something";
+            ViewBag.Address = "Mirpur Azad Kashmir";
+            return View();
+        }
+        public ActionResult GetEmployee()
+        {
+            List<Employee> employees = new List<Employee>()
+
+            {
+            new Employee()
+            {
+                EmployeeId = 1,
+                EmployeeName = "mark smith",
+                Address = "London",
+                DateOfJoining = System.DateTime.Now,
+                MartialStatus = 1,
+                IsEligibleForLoad = true,
+                Salary = 15000.00m,
+                CreatedBy = "Admin",
+                CreatedDate = System.DateTime.Now
+            },
+            new Employee()
+            {
+                EmployeeId = 1,
+                EmployeeName = "John smith",
+                Address = "London",
+                DateOfJoining = System.DateTime.Now,
+                MartialStatus = 1,
+                IsEligibleForLoad = true,
+                Salary = 15000.00m,
+                CreatedBy = "Admin",
+                CreatedDate = System.DateTime.Now
+            },
+
+            new Employee()
+            {
+                EmployeeId = 1,
+                EmployeeName = "Dunk smith",
+                Address = "London",
+                DateOfJoining = System.DateTime.Now,
+                MartialStatus = 1,
+                IsEligibleForLoad = true,
+                Salary = 15000.00m,
+                CreatedBy = "Admin",
+                CreatedDate = System.DateTime.Now
+            },
+
+            new Employee()
+            {
+                EmployeeId = 1,
+                EmployeeName = "mikelson",
+                Address = "London",
+                DateOfJoining = System.DateTime.Now,
+                MartialStatus = 1,
+                IsEligibleForLoad = true,
+                Salary = 15000.00m,
+                CreatedBy = "Admin",
+                CreatedDate = System.DateTime.Now
+            },
+
+            new Employee()
+            {
+                EmployeeId = 1,
+                EmployeeName = "Damon salvatore",
+                Address = "London",
+                DateOfJoining = System.DateTime.Now,
+                MartialStatus = 1,
+                IsEligibleForLoad = true,
+                Salary = 15000.00m,
+                CreatedBy = "Admin",
+                CreatedDate = System.DateTime.Now
+            }
+            };
+
+
+            ViewBag.Employees = employees;
+            return View();
+
+        }
+    }
+}
---
+++ videocode/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace videocode.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string Address { get; set; }
+        public DateTime DateOfJoining { get; set; }
+        public int MartialStatus { get; set; }
+        public bool IsEligibleForLoad { get; set; }
+        public decimal Salary { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
